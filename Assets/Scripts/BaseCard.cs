b444dc8c71bc20d3e46c712946e55fd1fe9abdb0
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2023-03-07 16:14:37 +0100

    First commit

---
+++ Assets/Scripts/BaseCard.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCard : MonoBehaviour
+{
+    public int cost;
+    public bool minion;
+
+    public int health;
+    public int strength;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
